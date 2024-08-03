f875ed4063cfeca35771e488f215236997a559fe
sans-byte <sanskarj15@gmail.com>
2024-08-03 21:02:14 +0530

    Added logic to filter the todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,99 @@
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 import "./App.css";
+import { todoAtom, todoSelector } from "./store/atom/todo";
+import React, { useRef } from "react";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RecoilRoot>
+        <TodoList />
+      </RecoilRoot>
+    </div>
+  );
 }
+
+function TodoList() {
+  const [todos, setTodos] = useRecoilState(todoAtom);
+  const todoRef = useRef();
+  const descriptionRef = useRef();
+  const filterRef = useRef();
+
+  const handleFilter = () => {
+    const filteredTodos = todos.filter(
+      (todo) =>
+        todo.title.match(new RegExp(filterRef.current.value, "i")) ||
+        todo.description.match(new RegExp(filterRef.current.value, "i"))
+    );
+    console.log(filterRef.current.value);
+    console.log(filteredTodos);
+    setTodos(filteredTodos);
+  };
+
+  const handleSubmit = () => {
+    const key = todos.length + 1;
+    setTodos([
+      ...todos,
+      {
+        key,
+        title: [todoRef.current.value],
+        description: [descriptionRef.current.value],
+      },
+    ]);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          ref={todoRef}
+          placeholder="Letss complete it"
+        />
+        <input
+          type="text"
+          name="description"
+          id="description"
+          ref={descriptionRef}
+          placeholder="Add description"
+        />
+        <button onClick={handleSubmit}> Add todo</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="filter"
+          id="filter"
+          ref={filterRef}
+          placeholder="Add filter"
+        />
+        <button onClick={handleFilter}> Filter todos</button>
+      </div>
+      {todos.map((todo, index) => (
+        <Todos todo={todo} key={index} />
+      ))}
+    </div>
+  );
+}
+
+const Todos = React.memo(function Todos({ todo }) {
+  return (
+    <div>
+      <h3>
+        <span>{todo.key}. </span>
+        {todo.title}
+      </h3>
+      <p>{todo.description}</p>
+    </div>
+  );
+});
 
 export default App;
--- frontend/src/store/atom/todo.js
+++ frontend/src/store/atom/todo.js
@@ -2,10 +2,16 @@ import { atom, selector } from "recoil";
 
 export const todoAtom = atom({
   key: "todoAtom",
-  default: [],
+  default: [
+    {
+      key: 1,
+      title: "Make lunch",
+      description: "Today is your turn to make the lunch so get to it",
+    },
+  ],
 });
 
-export const todoSelector = atom({
+export const todoSelector = selector({
   key: "todoSelector",
   get: (props) => {
     const todo = props.get(todoAtom);
